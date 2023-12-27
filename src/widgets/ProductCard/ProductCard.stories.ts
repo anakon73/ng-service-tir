a23af133e73e4f1b977903d1bdbe3bf22144c957
anakon73 <nazarkinah73@gmail.com>
2023-12-27 21:21:50 +0200

    docs(ProductCard): describe stories

---
+++ src/widgets/ProductCard/ProductCard.stories.ts
@@ -0,0 +1,86 @@
+import { Meta, StoryObj } from '@storybook/angular'
+import { moduleMetadata } from '@storybook/angular/dist/client/decorators'
+import { argsToTemplate } from '@storybook/angular/dist/client/argsToTemplate'
+import { CommonModule } from '@angular/common'
+
+import { LikeButtonModule } from '../../features/LikeButton/LikeButton.module'
+import { RatingModule } from '../../features/Rating/Rating.module'
+
+import { StarModule } from '../../entities/Star/Star.module'
+
+import { UILib } from '../../shared/ui/UILib.module'
+import { IProduct } from '../../shared/types'
+
+import { ProductCard } from './ProductCard.component'
+import disk from './assets/disk.png'
+
+const product: IProduct = {
+  name: 'Распредвал для Scania',
+  description: `
+      Это текст-"рыба", часто используемый в печати и
+      вэб-дизайне. Lorem Ipsum
+      является стандартной "рыбой" для текстов на латинице
+      с начала XVI века.
+    `,
+  discount: true,
+  image: disk,
+  price: 2200,
+  rate: {
+    quantity: 127,
+    rate: 4.8,
+  },
+  priceWithDiscount: 2000,
+}
+
+const meta: Meta<ProductCard> = {
+  title: 'widgerts/ProductCard',
+  component: ProductCard,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        LikeButtonModule,
+        StarModule,
+        CommonModule,
+        RatingModule,
+        UILib,
+      ],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+      <div class="w-1/4">
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+      </div>
+    `,
+  }),
+}
+
+export default meta
+
+export const Default: StoryObj<ProductCard> = {
+  args: {
+    product,
+  },
+}
+
+export const Grid: StoryObj<ProductCard> = {
+  args: {
+    product,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div class="grid grid-cols-4 gap-2 max-w-[1140px] mb-10">
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+        <ProductCard ${argsToTemplate(args)}></ProductCard>
+      </div>
+    `,
+  }),
+}

203296f290a872a5aa0b42ae9baa4aeda8ae3bcf
anakon73 <nazarkinah73@gmail.com>
2023-12-27 23:08:42 +0200

    docs(storybook): alias imports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular'
 
+const path = require('path')
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -17,6 +19,17 @@ const config: StorybookConfig = {
   core: {
     //@ts-ignore
     builder: '@storybook/builder-vite',
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/app': path.resolve(__dirname, '../src/app'),
+      '@/shared': path.resolve(__dirname, '../src/shared'),
+      '@/entities': path.resolve(__dirname, '../src/entities'),
+      '@/features': path.resolve(__dirname, '../src/features'),
+      '@/widgets': path.resolve(__dirname, '../src/widgets'),
+    }
+    return config
   },
 }
 export default config

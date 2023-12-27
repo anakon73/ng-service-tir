1374b0a36e032298fb8e0dfa90c8e7a2369a3c63
anakon73 <nazarkinah73@gmail.com>
2023-12-27 21:19:57 +0200

    docs(Rating): describe stories

---
+++ src/features/Rating/Rating.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/angular'
+import { moduleMetadata } from '@storybook/angular/dist/client/decorators'
+import { CommonModule } from '@angular/common'
+
+import { StarModule } from '../../entities/Star/Star.module'
+
+import { Rating } from './Rating.component'
+
+const meta: Meta<Rating> = {
+  title: 'features/Rating',
+  component: Rating,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, StarModule],
+    }),
+  ],
+}
+
+export default meta
+
+export const Default: StoryObj<Rating> = {
+  args: {
+    rating: {
+      quantity: 127,
+      rate: 4.8,
+    },
+  },
+}

d99a400c7328ddf78e035598bbc3bb820ac315ba
anakon73 <nazarkinah73@gmail.com>
2023-12-28 13:25:56 +0200

    docs(ServiceCard): describe stories

---
+++ src/widgets/ServiceCard/ServiceCard.stories.ts
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/angular'
+import { moduleMetadata } from '@storybook/angular/dist/client/decorators'
+import { argsToTemplate } from '@storybook/angular/dist/client/argsToTemplate'
+import { CommonModule } from '@angular/common'
+
+import { IService } from '@/shared/types'
+import { UILib } from '@/shared/ui/UILib.module'
+
+import clean from './assets/clean.png'
+import { ServiceCard } from '.'
+
+const service: IService = {
+  name: 'Дезинфекция и озонирование автомобилей и автобусов',
+  description: `
+    Это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem
+    Ipsum является
+  `,
+  image: clean,
+}
+
+const meta: Meta<ServiceCard> = {
+  title: 'widgets/ServiceCard',
+  component: ServiceCard,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, UILib],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+      <ServiceCard ${argsToTemplate(args)}></ServiceCard>
+    `,
+  }),
+}
+
+export default meta
+
+export const Default: StoryObj<ServiceCard> = {
+  args: {
+    service: service,
+  },
+}

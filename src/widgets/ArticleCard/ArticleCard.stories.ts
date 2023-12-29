720d184cb6755ab348c11e58e0291f7baa86328a
anakon73 <nazarkinah73@gmail.com>
2023-12-29 17:29:14 +0200

    docs(ArticleCard): describe stories

---
+++ src/widgets/ArticleCard/ArticleCard.stories.ts
@@ -0,0 +1,63 @@
+import { Meta, StoryObj } from '@storybook/angular'
+import { NgIconsModule } from '@ng-icons/core'
+import { heroArrowUpRight } from '@ng-icons/heroicons/outline'
+
+import { IArticle } from '@/shared/types'
+import { argsToTemplate, moduleMetadata } from '@/shared/lib'
+
+import engine from './assets/engine.png'
+import { ArticleCard } from '.'
+
+const article: IArticle = {
+  date: '1 Вересня 2023',
+  description: `
+    Like to know the secrets of transforming a 2-14 team into a 3x Super
+    Bowl winning Dynasty?`,
+  image: engine,
+  name: 'Як відбувається ремонт двигунів?',
+}
+
+const meta: Meta<ArticleCard> = {
+  title: 'widgets/ArticleCard',
+  component: ArticleCard,
+  decorators: [
+    moduleMetadata({
+      imports: [NgIconsModule.withIcons({ heroArrowUpRight })],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+      <div>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+      </div>
+    `,
+  }),
+}
+
+export default meta
+
+export const Default: StoryObj<ArticleCard> = {
+  args: {
+    article,
+  },
+}
+
+export const Row: StoryObj<ArticleCard> = {
+  args: {
+    article,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div class="grid grid-cols-3 gap-10 max-w-[1140px]">
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+        <ArticleCard ${argsToTemplate(args)}></ArticleCard>
+      </div>
+    `,
+  }),
+}

684ac5bc902042856c9e493b03a43c51546668f3
anakon73 <nazarkinah73@gmail.com>
2023-12-29 18:21:10 +0200

    docs(Rating): add story variant for withoutQuantity

--- src/entities/Product/Rating/Rating.stories.ts
+++ src/entities/Product/Rating/Rating.stories.ts
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from '@storybook/angular'
 import { CommonModule } from '@angular/common'
 
-import { UILib } from '@/shared/ui/UILib.module'
+import { UILib } from '@/shared/ui'
 import { moduleMetadata } from '@/shared/lib'
 
 import { Rating } from '.'
@@ -9,6 +9,16 @@ import { Rating } from '.'
 const meta: Meta<Rating> = {
   title: 'entities/Product/Rating',
   component: Rating,
+  argTypes: {
+    withQuantity: { type: 'boolean' },
+  },
+  args: {
+    withQuantity: true,
+    rating: {
+      quantity: 127,
+      rate: 4.8,
+    },
+  },
   decorators: [
     moduleMetadata({
       imports: [CommonModule, UILib],
@@ -18,11 +28,10 @@ const meta: Meta<Rating> = {
 
 export default meta
 
-export const Default: StoryObj<Rating> = {
+export const Default: StoryObj<Rating> = {}
+
+export const WithoutQuantity: StoryObj<Rating> = {
   args: {
-    rating: {
-      quantity: 127,
-      rate: 4.8,
-    },
+    withQuantity: false,
   },
 }

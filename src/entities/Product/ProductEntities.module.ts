e0838779bba9d4d5d484513bedd5b56cf56f433b
anakon73 <nazarkinah73@gmail.com>
2023-12-29 18:25:39 +0200

    fix(NgModules): current path to other modules

--- src/entities/Product/ProductEntities.module.ts
+++ src/entities/Product/ProductEntities.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { UILib } from '@/shared/ui/UILib.module'
+import { UILib } from '@/shared/ui'
 
 import { Rating } from './Rating'
 
--- src/widgets/ProductCard/ProductCard.module.ts
+++ src/widgets/ProductCard/ProductCard.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { ProductEntitiesModule } from '@/entities/Product/ProductEntities.module'
+import { ProductEntitiesModule } from '@/entities/Product'
 
-import { ProductFeaturesModule } from '@/features/Product/ProductFeatures.module'
+import { ProductFeaturesModule } from '@/features/Product'
 
-import { UILib } from '@/shared/ui/UILib.module'
+import { UILib } from '@/shared/ui'
 
 import { ProductCard } from '.'
 

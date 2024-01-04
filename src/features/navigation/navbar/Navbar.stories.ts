18e36ef9c025ce82ba6d2968263661d76874ba0f
anakon73 <nazarkinah73@gmail.com>
2024-01-04 16:24:37 +0200

    docs(Navbar): describe stories

---
+++ src/features/navigation/navbar/Navbar.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular'
+
+import { Navbar } from '.'
+
+const meta: Meta<Navbar> = {
+  title: 'features/Navigation/Navbar',
+  component: Navbar,
+}
+
+export default meta
+
+export const Default: StoryObj<Navbar> = {}

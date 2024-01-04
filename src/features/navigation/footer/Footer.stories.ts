a98ac96fc068e717e2dcaec58826a851cbac923a
anakon73 <nazarkinah73@gmail.com>
2024-01-04 16:17:31 +0200

    docs(Footer): describe stories

---
+++ src/features/navigation/footer/Footer.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular'
+
+import { Footer } from '.'
+
+const meta: Meta<Footer> = {
+  title: 'features/Product/Footer',
+  component: Footer,
+}
+
+export default meta
+
+export const Default: StoryObj<Footer> = {}

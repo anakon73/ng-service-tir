8878f331e4c9a54d09c92f5b8f12da144b873dfd
anakon73 <nazarkinah73@gmail.com>
2024-01-05 16:52:10 +0200

    refactor(app): remove html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { Header } from '@/widgets/Header'
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header],
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {}
